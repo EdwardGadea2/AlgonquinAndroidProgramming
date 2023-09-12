3a8d1d6ffc35f48d145eb488d4e863f72f93491e
EdwardGadea2 <gade0007@algonquinlive.com>
2023-09-12 15:54:29 -0400

    Hello!

--- app/src/main/java/algonquin/cst2335/gade0007/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/gade0007/MainActivity.java
@@ -8,9 +8,12 @@ import android.os.Bundle;
 // hello :)
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    // Same as psvm (String[] args)...
+    @Override   // aka its the starting point of the app
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // this is the only function! this loads stuff onto the screen
         setContentView(R.layout.activity_main);
     }
 }
