c0eafacf05267bd7f108a77053390c15f4e6a761
EdwardGadea2 <gade0007@algonquinlive.com>
2023-09-20 15:24:23 -0400

    Commit 7: Finished week 2; added in compound buttons, an imageview, and an imagebutton.

--- app/src/main/java/algonquin/cst2335/gade0007/ui/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/gade0007/ui/MainActivity.java
@@ -5,8 +5,14 @@ import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import algonquin.cst2335.gade0007.data.MainViewModel;
 import algonquin.cst2335.gade0007.databinding.ActivityMainBinding;
@@ -26,12 +32,10 @@ public class MainActivity extends AppCompatActivity {
 
         model = new ViewModelProvider(this).get(MainViewModel.class);
 
-        // this is the only function! this loads stuff onto the screen
-//        setContentView(R.layout.activity_main);
         variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(variableBinding.getRoot());
 
-
+        // initialize variables with each element
 //        TextView mytextview = findViewById(R.id.theTextView);
 //        Button mybutton = findViewById(R.id.theButton);
 //        EditText myedittext = findViewById(R.id.theEditText);
@@ -39,15 +43,67 @@ public class MainActivity extends AppCompatActivity {
         Button mybutton = variableBinding.theButton;
         EditText myedittext = variableBinding.theEditText;
 
-        model.editString.observe(this, s -> {
-            mytextview.setText(s);
+        // initialize variables for the compound buttons
+        CheckBox mycheckbox = variableBinding.checkBox;
+        RadioButton myradiobutton = variableBinding.radioButton;
+        Switch myswitch1 = variableBinding.switch1;
+
+        // initialize variables for the image elements
+        ImageView myimageview = variableBinding.theImageView;
+        ImageButton myimagebutton = variableBinding.theImageButton;
+
+        // upon screen rotation, keeps the textview message as the value given in the edittext
+        model.editString.observe(this, selected -> {
+            mytextview.setText(selected);
         });
 
+        // if the button is pressed, the value entered in the edittext will be placed in the textview, replacing Hello World
+        // can survive rotation
         if(mybutton != null) mybutton.setOnClickListener( vw -> {
             model.editString.postValue(myedittext.getText().toString());
-            model.editString.observe(this, s -> {
-                mytextview.setText("Your edit text has: " + s);
+            model.editString.observe(this, selected -> {
+                mytextview.setText("Your edit text has: " + selected);
             });
+        });
+
+        // if the checkbox is selected, all other compound buttons will be selected too, and a toast will appear with the current state of the buttons
+        mycheckbox.setOnCheckedChangeListener( (btn, isChecked) -> {
+            // save the current state of the button
+            model.isSelected.postValue(isChecked);
+            // look at what the current state of the button is, and set the other 2 compound buttons to that state
+            model.isSelected.observe(this, selected -> {
+                myradiobutton.setChecked(selected);
+                myswitch1.setChecked(selected);
+            });
+            // the toast
+            Toast toastCB = Toast.makeText(this /* MyActivity */, "The value is now: " + isChecked, Toast.LENGTH_SHORT);
+            toastCB.show();
+        });
+        // if the radiobutton is selected, similar to checkbox, all other compound buttons will be selected
+        myradiobutton.setOnCheckedChangeListener( (btn, isChecked) -> {
+            model.isSelected.postValue(isChecked);
+            model.isSelected.observe(this, selected -> {
+                mycheckbox.setChecked(selected);
+                myswitch1.setChecked(selected);
+            });
+            Toast toastRB = Toast.makeText(this /* MyActivity */, "The value is now: " + isChecked, Toast.LENGTH_SHORT);
+            toastRB.show();
+        });
+        // if the switch is selected, every other compound button gets selected
+        myswitch1.setOnCheckedChangeListener( (btn, isChecked) -> {
+            model.isSelected.postValue(isChecked);
+            model.isSelected.observe(this, selected -> {
+                myradiobutton.setChecked(selected);
+                mycheckbox.setChecked(selected);
+            });
+            Toast toastS = Toast.makeText(this /* MyActivity */, "The value is now: " + isChecked, Toast.LENGTH_SHORT);
+            toastS.show();
+        });
+
+        // on click event for the image button; if it's selected, a toast will appear with the dimensions of the image
+        myimagebutton.setOnClickListener( vw -> {
+            Toast toastIB = Toast.makeText(this /* MyActivity */, "Width = " + myimagebutton.getWidth() + " and Height = " + myimagebutton.getHeight(), Toast.LENGTH_SHORT);
+            toastIB.show();
         });
     }
 }
